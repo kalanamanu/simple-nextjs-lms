d78dfbb4df8036427d629ea5f20c21a1d846a2a0
kalanamanu <kalanamanupiya32@gmail.com>
2026-01-13 00:54:33 +0530

    Add MongoDB connection utility
    Introduces a db.js file with a function to connect to MongoDB using Mongoose, handling connection errors and exporting the utility for use in the backend.

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env, Mongo_URI);
+        console.log("DB Connected")
+    } catch (error) {
+        console.error("DB connection failed:", error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
